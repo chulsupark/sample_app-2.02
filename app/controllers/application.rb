8da9a1aea033c1bb51b981d3187761ab667fae77
Chulsu Park <chulsupark@gmail.com>
2011-10-23 23:16:38 -0400

    User signup complete

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,4 +7,7 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'd3480c51c37ef691431610d26bbc6a77'
+  
+  # Scrub sensitive parameters from your log
+  filter_parameter_logging :password
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
 
-  def new
-    @title = "Sign up"
-  end
-  
    def show
     @user = User.find(params[:id])
     @title = @user.name
   end
+  
+  def new
+    @title = "Sign up"
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render :action => :new
+    end
+  end
+
 end
